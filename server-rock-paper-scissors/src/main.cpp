2002b5aa79064bd4374b46359e2e25a8d3e80690
Tommy4chan <tommy4chan@gmail.com>
2024-11-04 13:04:48 +0200

    server code optimisation

--- server-rock-paper-scissors/src/main.cpp
+++ server-rock-paper-scissors/src/main.cpp
@@ -2,6 +2,7 @@
 
 JsonDocument doc;
 String jsonString;
+JsonDocument gameOutcome;
 
 String randomChoice()
 {
@@ -23,12 +24,18 @@ String randomChoice()
 void setup()
 {
 	Serial.begin(9600);
+	gameOutcome["RockScissors"] = 1;
+	gameOutcome["RockPaper"] = 2;
+	gameOutcome["ScissorsRock"] = 2;
+	gameOutcome["ScissorsPaper"] = 1;
+	gameOutcome["PaperRock"] = 1;
+	gameOutcome["PaperScissors"] = 2;
 }
 
 void loop()
 {
 	while (Serial.available())
-	{							
+	{
 		char c = Serial.read();
 
 		jsonString += c;
@@ -37,6 +44,7 @@ void loop()
 		{
 			DeserializationError error = deserializeJson(doc, jsonString);
 			JsonDocument response;
+
 			String resp = "";
 
 			if (error)
@@ -48,49 +56,34 @@ void loop()
 			}
 
 			jsonString = "";
-			
+
+			String player1Choice = doc["player1Choice"];
+			String player2Choice = doc["player2Choice"];
 
 			if (!doc["player1Choice"])
 			{
-				doc["player1Choice"] = randomChoice();
+				player1Choice = randomChoice();
 			}
-			
+
 			if (!doc["player2Choice"])
 			{
-				doc["player2Choice"] = randomChoice();
+				player2Choice = randomChoice();
 			}
 
-			if (doc["player1Choice"] == doc["player2Choice"])
+			int winner;
+			if (player1Choice == player2Choice)
 			{
-				response["winner"] = 3;
+				winner = 3;
 			}
-			else if (doc["player1Choice"] == "Rock" && doc["player2Choice"] == "Scissors")
+			else
 			{
-				response["winner"] = 1;
-			}
-			else if (doc["player1Choice"] == "Rock" && doc["player2Choice"] == "Paper")
-			{
-				response["winner"] = 2;
-			}
-			else if (doc["player1Choice"] == "Scissors" && doc["player2Choice"] == "Rock")
-			{
-				response["winner"] = 2;
-			}
-			else if (doc["player1Choice"] == "Scissors" && doc["player2Choice"] == "Paper")
-			{
-				response["winner"] = 1;
-			}
-			else if (doc["player1Choice"] == "Paper" && doc["player2Choice"] == "Rock")
-			{
-				response["winner"] = 1;
-			}
-			else if (doc["player1Choice"] == "Paper" && doc["player2Choice"] == "Scissors")
-			{
-				response["winner"] = 2;
+				winner = gameOutcome[player1Choice + player2Choice];
 			}
 
-			response["player1Choice"] = doc["player1Choice"];
-			response["player2Choice"] = doc["player2Choice"];
+			response["winner"] = winner;
+
+			response["player1Choice"] = player1Choice;
+			response["player2Choice"] = player2Choice;
 
 			serializeJson(response, resp);
 
